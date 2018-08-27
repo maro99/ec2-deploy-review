9b41674e0b220b1e6ad81bcf3cff68219a9ddee1
nadcdc <nadcdc4@gmail.com>
2018-08-27 20:26:21 +0900

    deploy-uwsgi.sh 작성.  runserver 대신 uwsgi 사용해서 서버상에서 런서버해봄.
     실행 시 서버에서 이미 실행중이던 uwsgi프로세스들을 모두 종료하고
     기존 deploy.sh 루틴 실행
     이후 runserver대신 uwsgi를 실행 (ini파일을 사용)

--- app/deploy-uwsgi.sh
+++ app/deploy-uwsgi.sh
@@ -10,9 +10,9 @@ SERVER_DIR='/home/ubuntu/project'
 # ssh로 서버에 접속하는 명령어
 CMD_CONNECT="ssh -i ${IDENTITY_FILE} ${USER}@${HOST}"
 
-#서버에서 실행중이던 runserver 프로세스들을 모두 종료
-${CMD_CONNECT} "pkill -9 -ef runserver"
-echo "kill runserver process"
+#서버에서 실행중이던 uwsgi 프로세스들을 모두 종료
+${CMD_CONNECT} "pkill -9 -ef uwsgi"
+echo "kill uwsgi process"
 
 
 # 서버의 파일을 지움.
@@ -34,10 +34,10 @@ echo "=GET Python path ($PYTHON_PATH)"
 
 
 # runserver를 background에서 실행해주는 커맨드 (nohup)
-RUNSERVER_CMD="nohup ${PYTHON_PATH} manage.py runserver 0:8000 &>/dev/null &"
+RUNSERVER_CMD="nohup ${VENV_PATH}/bin/uwsgi --ini .config/uwsgi_http2.ini &>/dev/null &"
 
-#서버 좁속 후, 프로젝트의 'app'폴더까지 이동한후 runserver 명령어를 실행
-${CMD_CONNECT} "cd ${SERVER_DIR}/app && ${RUNSERVER_CMD}"
+#서버 좁속 후, 프로젝트의 폴더까지 이동한후 runserver 명령어를 실행
+${CMD_CONNECT} "cd ${SERVER_DIR} && ${RUNSERVER_CMD}"
 echo  "Execute runserver"
 
 echo "Deploy complete"
