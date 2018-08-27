8a220a8ac8d9f358a20263a4a24bef00167d2f19
nadcdc <nadcdc4@gmail.com>
2018-08-27 19:04:33 +0900

    STATIC_ROOT 지정, collect static, 해서 .static에 static파일 모음. uwsgi 설치 및 테스트 해봄--->static파일 적용 안되는것 확인됨.

--- app/config/settings.py
+++ app/config/settings.py
@@ -14,8 +14,19 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-TEMPLATES_DIR=os.path.join(BASE_DIR,'templates')
-STATICFILES_DIR=os.path.join(BASE_DIR,'static')
+TEMPLATES_DIR = os.path.join(BASE_DIR,'templates')
+STATICFILES_DIR = os.path.join(BASE_DIR,'static')
+
+ROOT_DIR = os.path.dirname(BASE_DIR)
+STATIC_ROOT = os.path.join(ROOT_DIR, '.static')
+
+print(os.path.abspath(__file__))
+print(BASE_DIR)
+print(ROOT_DIR)
+print(STATICFILES_DIR)
+print(STATIC_ROOT)
+
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
